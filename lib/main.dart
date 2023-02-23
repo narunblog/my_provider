89d7840bb286e7e66e14ce4a268340f4a432215c
Your Name <you@example.com>
2023-02-23 16:08:41 +0900

    seconde

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_provider/models/counter_model.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
@@ -21,7 +22,9 @@ class MyApp extends StatelessWidget {
       home: ChangeNotifierProvider<CounterModel>(
         create: (_) => CounterModel(),
         child: Scaffold(
-          appBar: AppBar(title: Text('Flutter Demo Home Page')),
+          appBar: AppBar(
+            title: Text('Flutter Demo Home Page'),
+          ),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -30,6 +33,20 @@ class MyApp extends StatelessWidget {
                   'You have pushed the button this many times:',
                 ),
                 CounterText(),
+                ChangeNotifierProvider(
+                  create: (_) => Counter(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      TextWidgetA(),
+                      TextWidgetB(),
+                      TextWidgetC(),
+                      ButtonWidgetA(),
+                      ButtonWidgetB(),
+                      ButtonWidgetC()
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
@@ -53,5 +70,92 @@ class CounterText extends StatelessWidget {
       Provider.of<CounterModel>(context).count.toString(),
       style: Theme.of(context).textTheme.displaySmall,
     );
+  }
+}
+
+class TextWidgetA extends StatelessWidget {
+  const TextWidgetA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Built TextWidgetA');
+
+    return Center(
+        child: Text('Counter A: ${context.watch<Counter>().countA}',
+            style: const TextStyle(fontSize: 20)));
+  }
+}
+
+class TextWidgetB extends StatelessWidget {
+  const TextWidgetB({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Built TextWidgetB');
+
+    return Center(
+        child: Text('Counter B: ${context.read<Counter>().countB}',
+            style: const TextStyle(fontSize: 20)));
+  }
+}
+
+class TextWidgetC extends StatelessWidget {
+  const TextWidgetC({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Built TextWidgetC');
+
+    return Center(
+        child: Text(
+            'Counter C: ${context.select((Counter counter) => counter.countC)}',
+            style: const TextStyle(fontSize: 20)));
+  }
+}
+
+class ButtonWidgetA extends StatelessWidget {
+  const ButtonWidgetA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: ElevatedButton(
+            onPressed: () {
+              context.read<Counter>().incrementCounterA();
+            },
+            child: const Text('Increment Count A',
+                style: TextStyle(fontSize: 20))));
+  }
+}
+
+class ButtonWidgetB extends StatelessWidget {
+  const ButtonWidgetB({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: ElevatedButton(
+            onPressed: () {
+              context.read<Counter>().incrementCounterB();
+            },
+            child: const Text('Increment Count B',
+                style: TextStyle(fontSize: 20))));
+  }
+}
+
+class ButtonWidgetC extends StatelessWidget {
+  const ButtonWidgetC({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Built ButtonWidgetC');
+
+    return Center(
+        child: ElevatedButton(
+            onPressed: () {
+              context.read<Counter>().incrementCounterC();
+            },
+            child: const Text('Increment Count C',
+                style: TextStyle(fontSize: 20))));
   }
 }
